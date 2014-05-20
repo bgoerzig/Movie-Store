86b6612ca4a9d3d5f1b62538c4d21116acc45ba9
bgoerzig <bgoerzig@gmail.com>
2014-05-20 12:43:47 +0200

    Renaming redundant variables
    Replaced a temporary variable with a query to avoid to much parameters
    being passed around.

--- Customer.java
+++ Customer.java
@@ -19,26 +19,22 @@ class Customer {
     public String statement() {
     	double totalAmount = 0;
     	int frequentRenterPoints = 0;
-    	Enumeration rentals = this.rentals.elements();
+    	Enumeration rentals = _rentals.elements();
     	String result = "Rental Record for " + getName() + "\n";
     	while (rentals.hasMoreElements()) {
-	    	double thisAmount = 0;
 	    	Rental each = (Rental) rentals.nextElement();
-	    	/* Extraction of switch-statement to avoid overly long function,
-	    	new method amountFor(Rental) implemented */
-	    	thisAmount = each.getCharge();
 	    	frequentRenterPoints ++;
-	    	if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-	    	each.getDaysRented() > 1) frequentRenterPoints ++;
-	    	result += "\t" + each.getMovie().getTitle()+ "\t" +
-	    	String.valueOf(thisAmount) + "\n";
-	    	totalAmount += thisAmount;
+	    	if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+	    	&& each.getDaysRented() > 1) 
+	    		frequentRenterPoints ++;
+	    	result += 	"\t" + each.getMovie().getTitle()+ "\t" +
+				    	String.valueOf(each.getCharge()) + "\n";
+				    	totalAmount += each.getCharge();
     	}
-    	//add footer lines
     	result += "Amount owed is " + String.valueOf(totalAmount) +
     	"\n";
-    	result += "You earned " + String.valueOf(frequentRenterPoints) +
-    	" frequent renter points";
+    	result += "You earned " + String.valueOf(frequentRenterPoints)
+    	+ " frequent renter points";
     	return result;
     }
     //delegate to the new method
