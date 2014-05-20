d118aafffbbec9e3414a355eb72f0d93c35dde2a
Bastian Dupps <dupps@email.de>
2014-05-20 15:22:20 +0200

    replacing the conditional logic on price code with polymorphism

--- Rental.java
+++ Rental.java
@@ -13,33 +13,12 @@ class Rental {
     public Movie getMovie() {
         return movie;
     }
-    //extracted method from class Customer
+
     int getFrequentRenterPoints() {
-    	if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-    	getDaysRented() > 1)
-    		return 2;
-    	else
-    		return 1;
+    	return this.movie.getFrequentRenterPoints(this.daysRented);
     }
     
-    // move method in correct class
 	double getCharge() {
-		double result = 0;
-		switch (getMovie().getPriceCode()) {
-	    	case Movie.REGULAR:
-	    		result += 2;
-	    		if (getDaysRented() > 2)
-	    			result += (getDaysRented() - 2) * 1.5;
-	    		break;
-	    	case Movie.NEW_RELEASE:
-	    		result += getDaysRented() * 3;
-	    		break;
-	    	case Movie.CHILDRENS:
-	    		result += 1.5;
-	    		if (getDaysRented() > 3)
-	    			result += (getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return result;
+		return this.movie.getCharge(daysRented);
 	}
 }
