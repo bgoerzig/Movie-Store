920a4efdaff147469b3708d8f253ab7f4df42938
bgoerzig <bgoerzig@gmail.com>
2014-05-20 15:15:08 +0200

    Added HTML generator for statement
    Implemented a method for generating a HTML version of the string
    "statement".

--- Customer.java
+++ Customer.java
@@ -21,16 +21,14 @@ class Customer {
     	String result = "Rental Record for " + getName() + "\n";
     	while (rentals.hasMoreElements()) {
 	    	Rental each = (Rental) rentals.nextElement();
-	    	//show figures for this rental
 	    	result += "\t" + each.getMovie().getTitle()+ "\t" +
 	    	String.valueOf(each.getCharge()) + "\n";
     	}
-    	//add footer lines
-    	result += "Amount owed is " +
-    	String.valueOf(getTotalCharge()) + "\n";
-    	result += "You earned " +
-    	String.valueOf(getTotalFrequentRenterPoints()) +
-    	" frequent renter points";
+    	result += 	"Amount owed is " +
+    				String.valueOf(getTotalCharge()) + "\n";
+    	result += 	"You earned " +
+    				String.valueOf(getTotalFrequentRenterPoints()) +
+    				" frequent renter points";
     	return result;
     }
 
@@ -51,6 +49,23 @@ class Customer {
     	Rental each = (Rental) rentals.nextElement();
     	result += each.getCharge();
     	}
+    	return result;
+    }
+    
+    //new method for generating an HTML version of the statement String
+    public String htmlStatement() {
+    	Enumeration rentals = this.rentals.elements();
+    	String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+    	while (rentals.hasMoreElements()) {
+	    	Rental each = (Rental) rentals.nextElement();
+	    	result += 	each.getMovie().getTitle()+ ": " +
+	    				String.valueOf(each.getCharge()) + "<BR>\n";
+    	}
+    	result += 	"<P>You owe <EM>" + String.valueOf(getTotalCharge()) +
+    				"</EM><P>\n";
+    	result += "On this rental you earned <EM>" +
+    	String.valueOf(getTotalFrequentRenterPoints()) + 
+    	"</EM> frequent renter points<P>";
     	return result;
     }
 }
