1ff77e613d8c35f8f845deddc4bce578539f5046
Bastian Dupps <dupps@email.de>
2014-05-20 15:02:28 +0200

    replace temps with querys for total amount and frequent render points

--- Customer.java
+++ Customer.java
@@ -1,6 +1,5 @@
 package de.bitbizeps.MovieStore;
 
-
 import java.lang.*;
 import java.util.*;
 
@@ -16,27 +15,43 @@ class Customer {
     public String getName (){
         return name;
     };
+ 
     public String statement() {
-    	double totalAmount = 0;
-    	int frequentRenterPoints = 0;
     	Enumeration rentals = this.rentals.elements();
     	String result = "Rental Record for " + getName() + "\n";
     	while (rentals.hasMoreElements()) {
 	    	Rental each = (Rental) rentals.nextElement();
-	    	//Extracted the method, moving it to the correct class Rental
-	    	frequentRenterPoints += each.getFrequentRenterPoints();
-	    	result +=	"\t" + each.getMovie().getTitle()+ "\t" +
-	    				String.valueOf(each.getCharge()) + "\n";
-	    	totalAmount += each.getCharge();
+	    	//show figures for this rental
+	    	result += "\t" + each.getMovie().getTitle()+ "\t" +
+	    	String.valueOf(each.getCharge()) + "\n";
     	}
-    	result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-    	result += "You earned " + String.valueOf(frequentRenterPoints)
-    	+ " frequent renter points";
+    	//add footer lines
+    	result += "Amount owed is " +
+    	String.valueOf(getTotalCharge()) + "\n";
+    	result += "You earned " +
+    	String.valueOf(getTotalFrequentRenterPoints()) +
+    	" frequent renter points";
     	return result;
-   	}
-    
-    private double amountFor(Rental aRental) {
-    	return aRental.getCharge();
+    }
+
+    private int getTotalFrequentRenterPoints(){
+    	int result = 0;
+    	Enumeration rentals = this.rentals.elements();
+    	while (rentals.hasMoreElements()) {
+	    	Rental each = (Rental) rentals.nextElement();
+	    	result += each.getFrequentRenterPoints();
+    	}
+    	return result;
+    }
+
+    private double getTotalCharge() {
+    	double result = 0;
+    	Enumeration rentals = this.rentals.elements();
+    	while (rentals.hasMoreElements()) {
+    	Rental each = (Rental) rentals.nextElement();
+    	result += each.getCharge();
+    	}
+    	return result;
     }
 }
     
