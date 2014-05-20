cd6a847f9e81d30ff3b982ede24b26cf84bb16af
Bastian Dupps <dupps@email.de>
2014-05-20 12:23:34 +0200

    rename variables and move method
    moved method from customer class to rental class because it uses
    information from the rental class

--- Customer.java
+++ Customer.java
@@ -26,7 +26,7 @@ class Customer {
 	    	Rental each = (Rental) rentals.nextElement();
 	    	/* Extraction of switch-statement to avoid overly long function,
 	    	new method amountFor(Rental) implemented */
-	    	thisAmount = amountFor(each);
+	    	thisAmount = each.getCharge();
 	    	frequentRenterPoints ++;
 	    	if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
 	    	each.getDaysRented() > 1) frequentRenterPoints ++;
@@ -41,27 +41,9 @@ class Customer {
     	" frequent renter points";
     	return result;
     }
-    
-    /*Separated switch-statement, new function that consumes Rental instance 
-    and returns the new amount*/
-    private double amountFor(Rental each) {
-    	double thisAmount = 0;
-    	switch (each.getMovie().getPriceCode()) {
-	    	case Movie.REGULAR:
-	    		thisAmount += 2;
-	    		if (each.getDaysRented() > 2)
-	    			thisAmount += (each.getDaysRented() - 2) * 1.5;
-	    		break;
-	    	case Movie.NEW_RELEASE:
-	    		thisAmount += each.getDaysRented() * 3;
-	    		break;
-	    	case Movie.CHILDRENS:
-	    		thisAmount += 1.5;
-	    		if (each.getDaysRented() > 3)
-	    			thisAmount += (each.getDaysRented() - 3) * 1.5;
-    		break;
-    	}
-    	return thisAmount;
+    //delegate to the new method
+    private double amountFor(Rental aRental) {
+    	return aRental.getCharge();
     }
 }
     
